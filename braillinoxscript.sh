7a18cb3f097e9c6f52bea8d35a5353682150bd6d
hector.a.mendez <hector.a.mendez.l@gmail.com>
2015-11-18 00:51:20 -0600

    It is a first modification in a master script to install

--- braillinoxscript.sh
+++ braillinoxscript.sh
@@ -1,21 +1,29 @@
-#este sera el unico script que debas instalar los otros se instalan con este
-#Idea, concepcion y dcumentacion (mal hecha y todo) pero 
-#Hecho en su totalidad por Héctor Méndez con muchas ayudar de muchas personas
-#Licenciadas bajo GPLv3
-
-#Cuidar horario configurado
-#cuidar region horaria con soporte actualizado
-#que la instalacion se haga en un directorio padre.
-
-
 #!/bin/bash
+
+
+#This is a modification and restructuration of master instalation script
+#The Idea ist just apply permitions and type ./ in command line and wait for the instalation
+#For the moment the project is just manteined by Hector Mendez, i am in searching for developers and interrest people for the project
+#Licenced under GPLv3
+#Check the internet conmection
+#ping 8.8.8.8
+#if ping gives package dates, continue the proces, this part i do not have an idea about of how to do. 
+
+
+date
+#if date is not correct launch the applet to configure the hour 
+
+#installation must be in a pathern directory
+
+
+
 cd
 ./soportegcc.sh
-echo "ya quedo gcc ahora a añadir el soporte de audio"
+echo "gcc is already installed"
+echo "audio suppor will be installed comming son"
 cd
 ./soporteaudio.sh
-echo "al parecer el proceso ha terminado"
-echo "ten cuidad pues este scriot sólo instala, no es disgnosticador de fallas"
-echo "tampoco resuelve conflictos y descarga alrededor de 260mb"
-echo "es necesario configurar tu internet bien para agilizar el proceso"
-#Por lo pronto es esto
+echo "such sems the process already have finished"
+echo "at this point, you are be careful because the script is not tester of fails, ist just installed"
+echo "neither resolve conflicts about the download  and this download is about 260mb"
+
